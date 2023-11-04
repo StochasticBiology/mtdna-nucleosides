66ecdf715f87ab1efce54f1312c5410c0f695258
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2023-11-04 09:36:36 +0100

    Account for different run numbers for each pairing

--- analysis.R
+++ analysis.R
@@ -16,7 +16,7 @@ cellnum.col = 3
 new.df = data.frame()
 
 # loop through sheets in Excel file
-for(this.sheet in 2:6) {
+for(this.sheet in 2:8) {
   # sheet 2 has different coordinates for some info
   if(this.sheet == 2) {
     control.col = 4
@@ -62,6 +62,20 @@ ggplot(new.df, aes(x=cellnum, y=area, colour=line, label=condition)) +
   geom_text(data=new.df[new.df$condition=="baseline",], aes(x=cellnum, y=area, colour=line, label=condition), size=3, nudge_y = 3, alpha=1) +
   facet_wrap(~sheet) + theme_light() + theme(legend.position="none") + 
   xlab("Cell number") + ylab("mtDNA area")
+
+sf = 2
+for(i in 2:8) {
+  png(paste("scatter-", i, ".png", sep=""), width=400*sf, height=300*sf, res=72*sf)
+  sub = new.df[new.df$sheet == i,]
+  g.plot = ggplot(sub, aes(x=cellnum, y=area, colour=line, label=condition)) + 
+    geom_point(alpha=0.4) + geom_text_repel(size=2, alpha=0.4) +
+    geom_point(data=sub[sub$condition=="baseline",], aes(x=cellnum, y=area, colour=line, label=condition), alpha=1) + 
+    geom_text(data=sub[sub$condition=="baseline",], aes(x=cellnum, y=area, colour=line, label=condition), size=3, nudge_y = 2, alpha=1) +
+    theme_light() + theme(legend.position="none") + 
+    xlab("Cell number") + ylab("mtDNA area")
+  print(g.plot)
+  dev.off()
+}
 
 # Effect of nucleoside treatment as a function of FCS concentration
 ########
@@ -123,7 +137,8 @@ if(read.from.raw == TRUE) {
   df$Patcont[df$Patcont=="SavSty"] = "POLG2"
   df$Patcont[df$Patcont=="TWNKw"] = "TWNK"
   sub = df[c("Run", "Patcont", "Condition", "mtDNA.area")]
-  
+
+  write.csv(df, "full-data.csv")
   write.csv(sub, "reduced-data.csv")
 } else {
   sub = read.csv("reduced-data.csv")
@@ -176,21 +191,37 @@ interest.set = c("Baseline", "dT", "dC", "dCdT", "dAdCdGdT")
 sub.interest = sub[sub$Condition %in% interest.set,]
 ggplot(sub.interest, aes(x=Run, y=log(mtDNA.area), fill=Condition)) + geom_boxplot() + facet_wrap(~ Patcont)
 
+ggplot(sub.interest[sub.interest$Patcont=="TWNK",], aes(x=Run, y=log(mtDNA.area), fill=Condition)) + geom_boxplot() + facet_wrap(~ Patcont)
+ggplot(sub.interest[sub.interest$Patcont=="POLG1",], aes(x=Run, y=log(mtDNA.area), fill=Condition)) + geom_boxplot() + facet_wrap(~ Patcont)
+ggplot(sub.interest[sub.interest$Patcont=="Control.1",], aes(x=Run, y=log(mtDNA.area), fill=Condition)) + geom_boxplot() + facet_wrap(~ Patcont)
+ggplot(sub.test, aes(x=Run, y=mtDNA.area, fill=Condition)) + geom_boxplot()
+
+# LMM or LM for condition change individually per line, batched by runs
+
 res.df = data.frame()
 patcont.set = unique(sub.interest$Patcont)
 for(this.patcont in patcont.set) {
   for(i in 2:length(interest.set)) {
     this.interest = interest.set[i]
     sub.test = sub.interest[sub.interest$Patcont == this.patcont & (sub.interest$Condition == "Baseline" | sub.interest$Condition == this.interest),]
+    # only keep runs that have both baseline and condition of interest represented
+    keep.runs = c()
+    for(this.run in unique(sub.test$Run)) {
+      sub.sub.test = sub.test[sub.test$Run == this.run,]
+      if(length(unique(sub.sub.test$Condition)) > 1) {
+        keep.runs = c(keep.runs, this.run)
+      }
+    }
+    sub.test = sub.test[sub.test$Run %in% keep.runs,]
     if(length(unique(sub.test$Condition)) > 1) {
       if(length(unique(sub.test$Run)) > 1) {
         this.lmer = lmer(log(mtDNA.area) ~ Condition + (Condition | Run), data=sub.test)
         this.fit = summary(this.lmer)
-        res.df = rbind(res.df, data.frame(patcont=this.patcont, condition=this.interest, mean=this.fit$coefficients[2,1], sd=this.fit$coefficients[2,2]))
+        res.df = rbind(res.df, data.frame(patcont=this.patcont, method="LMM", condition=this.interest, mean=this.fit$coefficients[2,1], sd=this.fit$coefficients[2,2]))
       } else {
         this.lm = lm(log(mtDNA.area) ~ Condition, data=sub.test)
         this.fit = summary(this.lm)
-        res.df = rbind(res.df, data.frame(patcont=this.patcont, condition=this.interest, mean=this.fit$coefficients[2,1], sd=this.fit$coefficients[2,2]))
+        res.df = rbind(res.df, data.frame(patcont=this.patcont, method="LM", condition=this.interest, mean=this.fit$coefficients[2,1], sd=this.fit$coefficients[2,2]))
       }
     }
   }
@@ -198,28 +229,3 @@ for(this.patcont in patcont.set) {
 
 # compute p-values based on normal distribution of estimator
 res.df$p = pnorm(0, mean=abs(res.df$mean), sd=res.df$sd)*2
-
-ggplot(sub.interest[sub.interest$Patcont=="TWNK",], aes(x=Run, y=log(mtDNA.area), fill=Condition)) + geom_boxplot() + facet_wrap(~ Patcont)
-ggplot(sub.test, aes(x=Run, y=mtDNA.area, fill=Condition)) + geom_boxplot()
-
-### this isn't looking right!
-
-res.df = data.frame()
-patcont.set = unique(sub.interest$Patcont)
-for(this.patcont in patcont.set) {
-  for(i in 2:length(interest.set)) {
-    this.interest = interest.set[i]
-    sub.test = sub.interest[sub.interest$Patcont == this.patcont & (sub.interest$Condition == "Baseline" | sub.interest$Condition == this.interest),]
-    if(length(unique(sub.test$Condition)) > 1) {
-      if(length(unique(sub.test$Run)) > 1) {
-        this.lmer = lmer(log(mtDNA.area) ~ Condition + (Condition | Run), data=sub.test)
-        this.fit = summary(this.lmer)
-        res.df = rbind(res.df, data.frame(patcont=this.patcont, condition=this.interest, mean=this.fit$coefficients[2,1], sd=this.fit$coefficients[2,2]))
-      } else {
-        this.lm = lm(log(mtDNA.area) ~ Condition, data=sub.test)
-        this.fit = summary(this.lm)
-        res.df = rbind(res.df, data.frame(patcont=this.patcont, condition=this.interest, mean=this.fit$coefficients[2,1], sd=this.fit$coefficients[2,2]))
-      }
-    }
-  }
-}
